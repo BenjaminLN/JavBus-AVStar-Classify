0cd0289c1d548e69e7c9c0820f4c1b32bbdf34f8
BenjaminLN <47185574+BenjaminLN@users.noreply.github.com>
2023-12-06 21:43:39 +0800

    Update AVstar-Classify.py
    访问javbus默认跳到登录页面去了，修改headers可跳过登录步骤直接访问内容。
    headers从https://github.com/DIYgod/RSSHub/pull/13462/commits/d22177e11af5b16b29a586ad16834b622982fef2嫖过来的

--- AVstar-Classify.py
+++ AVstar-Classify.py
@@ -3,7 +3,6 @@ import win32clipboard as wc
 import win32con
 from requests import Session
 from bs4 import BeautifulSoup
-from requests import utils
 # 获取剪切板内容
 def getCopy():
     wc.OpenClipboard()
@@ -17,9 +16,20 @@ def setCopy(str):
     wc.SetClipboardData(win32con.CF_UNICODETEXT, str)
     wc.CloseClipboard()
 
-headers = {'User-Agent': 'Mozilla/5.0',}
+headers = {
+        'accept-language': 'zh-CN,zh;q=0.9,zh-HK;q=0.8',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Chromium";v="117", "Not;A=Brand";v="8"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+    };
 cookiesDit = {'existmag':'all'}
-url='https://www.javbus.com/star/nyi'
+url='https://www.javbus.com/star/ytb'
 flag=1
 result_list=[]
 
@@ -28,7 +38,7 @@ if url[-1]!='/':
 
 while True:
     request = Session()
-    request.proxies = {"https": 'http://127.0.0.1:1080/pac?auth=6sZoXM8Q3lZyeXZMnVP-&t=201909182318551455'}
+    request.proxies = {"https": '127.0.0.1:7890'}
     res=request.get(url+str(flag),headers=headers,cookies=cookiesDit)
     if res.status_code!=200:
         print(url + str(flag))
@@ -37,6 +47,7 @@ while True:
     res.content.decode("utf8","ignore").encode("gbk","ignore")
 
     soup=BeautifulSoup(res.text,'html.parser')
+    # print(soup)
     myfanhao=soup.find_all('date')
     for each in enumerate(myfanhao):
         if (each[0]+1)%2==1:
